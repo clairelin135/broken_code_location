b65865c98a4fa4dfe673fcd333e8a08fc72706d7
Claire Lin <claire@dagsterlabs.com>
2025-02-24 12:50:52 -0800

    make code location unloadable

--- quickstart_etl/definitions.py
+++ quickstart_etl/definitions.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from dagster import (
+    asset,
     Definitions,
     ScheduleDefinition,
     define_asset_job,
@@ -11,8 +12,6 @@ from dagster import (
     with_source_code_references,
 )
 from dagster._core.definitions.metadata.source_code import AnchorBasedFilePathMapping
-
-from . import assets
 
 daily_refresh_schedule = ScheduleDefinition(
     job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
@@ -28,25 +27,11 @@ def foo_op():
 def my_asset():
     return foo_op()
 
-
-my_assets = with_source_code_references(
-    [
-        my_asset,
-        *load_assets_from_package_module(assets),
-    ]
-)
-
-my_assets = link_code_references_to_git(
-    assets_defs=my_assets,
-    git_url="https://github.com/dagster-io/dagster/",
-    git_branch="master",
-    file_path_mapping=AnchorBasedFilePathMapping(
-        local_file_anchor=Path(__file__).parent,
-        file_anchor_path_in_repository="examples/quickstart_etl/quickstart_etl/",
-    ),
-)
+@asset(aslkdjaskld=5)
+def broken_asset():
+    return 1
 
 defs = Definitions(
-    assets=my_assets,
+    assets=[broken_asset],
     schedules=[daily_refresh_schedule],
 )
